e5f5fed9847aafcc067e7c63a4435ea4d29e2adc
litakk <aazizaliev2403@gmail.com>
2026-06-29 17:44:42 +0500

    fix build package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,29 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel"; // Импортируем адаптер для Vercel
+
+const isVercel = process.env.VERCEL === "1";
 
 export default defineConfig({
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  
+  adapter: isVercel 
+    ? vercel() 
+    : node({ mode: "standalone" }),
+
   integrations: [react()],
+  
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      rolldownOptions: {
+        output: {
+          codeSplitting: true
+        }
+      },
+      // Поднимаем лимит до 800кб, чтобы сборщик не ругался на тяжелые UI/карты
+      chunkSizeWarningLimit: 800
+    }
   },
 });
