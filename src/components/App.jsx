e87825cd3e5f4017cb4165163331bc4c642fa048
litakk <aazizaliev2403@gmail.com>
2026-07-04 18:54:10 +0500

    changed video to large size

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -56,7 +56,7 @@ export default function App({ locale, translations, currentPath }) {
             poster="/video/hero_poster.jpg"
             className="absolute inset-0 w-full h-full object-cover"
           >
-            <source src="/video/hero_video.mp4" type="video/mp4" />
+            <source src="/video/hero_video-Second.mp4" type="video/mp4" />
           </video>
           <div className="absolute inset-0 bg-black/60"></div>
 
